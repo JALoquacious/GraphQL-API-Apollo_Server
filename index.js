273a27c0d66460f79bebe583073bb6f12b24de64
J.A. Loquacious <jlkds@pm.me>
2021-12-24 14:34:20 -0500

    Expose Speaker API REST data source

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const { ApolloServer } = require('apollo-server');
-const SessionAPI = require('./datasources/sessions');
 
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+const SessionAPI = require('./datasources/sessions');
+const SpeakerAPI = require('./datasources/speakers');
 
 const dataSources = () => ({
-    sessionAPI: new SessionAPI()
+    sessionAPI: new SessionAPI(),
+    speakerAPI: new SpeakerAPI()
 });
 
-const server = new ApolloServer({ typeDefs, resolvers, dataSources });
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources
+});
 server
     .listen({ port: process.env.PORT || 4000 })
     .then(({ url }) => {
